9fde009766d1403d4a448ae62f7dd6cfc546d68c
Ezekiel <ezerivadeneiral@gmail.com>
2013-12-09 10:23:49 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CulturalGps::Application.config.secret_key_base = '08b714dede9229d183234883859540522a5d859a4af0f857e9c6116a636fe8f8c25bbdc75a685cb7c08b8c5ef47af3c2630e5600f8fcb4b2d221e5b4fa737461'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CulturalGps::Application.config.session_store :cookie_store, key: '_cultural-gps_session'
